b3bafc34ff964edcdbc18e349192aa117bbe6512
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-03-26 18:00:06 +0300

    prints board after player's move

--- src/main/java/reversi/application/Main.java
+++ src/main/java/reversi/application/Main.java
@@ -29,6 +29,7 @@ public class Main {
             int x;
             int y;
             do {
+                System.out.println(game.toString());
                 AI.makeNextMove(game);
                 System.out.println(game.toString());
                 System.out.println("It's " + getPlayer(game.getTurn()) + "'s turn.");
