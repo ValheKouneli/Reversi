3b303fc885c957ea45519ac0dcbaf03edf62b0e8
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-03-26 16:00:18 +0300

    made some test graphics and started to wonder how to build an AI

---
+++ src/main/java/reversi/AI/AI.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reversi.AI;
+
+import reversi.game.Game;
+
+/**
+ *
+ * @author Valhe Kouneli
+ */
+public class AI {
+    
+    public void makeNextMove(Game game) {
+        
+    }
+    
+}
