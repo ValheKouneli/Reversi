5eaa409149b1c051fdd787cdbd303aaf05d589ec
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-03-28 13:54:24 +0300

    made a general reverse method to be used by all the specific reverse methods

--- src/main/java/reversi/game/ReverseHelper.java
+++ src/main/java/reversi/game/ReverseHelper.java
@@ -15,6 +15,69 @@ public class ReverseHelper {
     
     private ReverseHelper() {}
     
+    private static boolean generalReverse(Board board, int turn, int x, int y,
+            int xDiff, int yDiff) {
+        
+        int limitOpp;
+        int limitOwn;
+        int orig;
+        int iterator;
+        int iteratorDiff;
+        if (xDiff != 0) {
+            limitOpp = x;
+            limitOwn = x;
+            orig = x;
+            iterator = x+xDiff;
+            iteratorDiff = xDiff;
+        } else {
+            limitOpp = y;
+            limitOwn = y;
+            orig = y;
+            iterator = y+yDiff;
+            iteratorDiff = yDiff;
+        }
+        
+        int i = x+xDiff;
+        int j = y+yDiff;
+        
+        while (0<=x && x<8 && 0<=y && y<8) {
+            if (board.getBoardXY(i, j) == turn*(-1)) {
+                if (xDiff != 0) {
+                    limitOpp = i;
+                } else {
+                    limitOpp = j;
+                }
+                
+            } else if (board.getBoardXY(i, j) == turn) {
+                if (xDiff != 0) {
+                    limitOwn = i;
+                } else {
+                    limitOwn = j;
+                }
+                break;
+            } else {
+                break;
+            }
+            i+=xDiff;
+            j+=yDiff;
+        }
+        
+        if (limitOpp!=orig && limitOwn!=orig) {
+            i = x+xDiff;
+            j = y+yDiff;
+            while (iterator != limitOwn) {
+                board.setBoardXY(i, j, turn);
+                i+=xDiff;
+                j+=yDiff;
+                iterator+=iteratorDiff;
+            }
+            return true; 
+        } else {
+            return false;
+        }
+
+    }
+    
     public static boolean reverseUp(Board board, int turn, int x, int y) {
         int limitOpp = x;
         int limitOwn = x;
