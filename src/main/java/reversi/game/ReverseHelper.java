9f953c5efde8ba205a47e5e6cf6daf7b6b2bfa0d
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-03-28 14:06:23 +0300

    Made all ReverseHelper methods use generalReverse method

--- src/main/java/reversi/game/ReverseHelper.java
+++ src/main/java/reversi/game/ReverseHelper.java
@@ -40,7 +40,7 @@ public class ReverseHelper {
         int i = x+xDiff;
         int j = y+yDiff;
         
-        while (0<=x && x<8 && 0<=y && y<8) {
+        while (0<=i && i<8 && 0<=j && j<8) {
             if (board.getBoardXY(i, j) == turn*(-1)) {
                 if (xDiff != 0) {
                     limitOpp = i;
@@ -79,240 +79,35 @@ public class ReverseHelper {
     }
     
     public static boolean reverseUp(Board board, int turn, int x, int y) {
-        int limitOpp = x;
-        int limitOwn = x;
-        for (int i=x-1; i>=0; i--) {
-            if (board.getBoardXY(i, y) == turn*(-1)) {
-                limitOpp = i;
-            } else if (board.getBoardXY(i, y) == turn) {
-                limitOwn = i;
-                break;
-            } else {
-                break;
-            }
-        }
-        
-        if (limitOpp<x && limitOwn<x) {
-            for (int i=x-1; i>limitOwn; i--) {
-                board.setBoardXY(i, y, turn);
-            }
-            return true;
-        } else {
-            return false;
-        }
+        return generalReverse(board, turn, x, y, -1, 0);
     }
     
     public static boolean reverseLeft(Board board, int turn, int x, int y) {
-        int limitOpp = y;
-        int limitOwn = y;
-        for (int j=y-1; j>=0; j--) {
-            if (board.getBoardXY(x, j) == turn*(-1)) {
-                limitOpp = j;
-            } else if (board.getBoardXY(x, j) == turn) {
-                limitOwn = j;
-                break;
-            } else {
-                break;
-            }
-        }
-        
-        if (limitOwn<y && limitOpp<y) {
-            for (int j=y-1; j>limitOwn; j--) {
-                board.setBoardXY(x, j, turn);
-            }
-            return true;
-        } else {
-            return false;
-        }
+        return generalReverse(board, turn, x, y, 0, -1);
     }
     
     public static boolean reverseDown(Board board, int turn, int x, int y) {
-        int limitOpp = x;
-        int limitOwn = x;
-        for (int i=x+1; i<8; i++) {
-            if (board.getBoardXY(i, y) == turn*(-1)) {
-                limitOpp = i;
-            } else if (board.getBoardXY(i, y) == turn) {
-                limitOwn = i;
-                break;
-            } else {
-                break;
-            }
-        }
-        
-        if (limitOwn>x && limitOpp>x) {
-            for (int i=x+1; i<limitOwn; i++) {
-                board.setBoardXY(i, y, turn);
-            }
-            return true;
-        } else {
-            return false;
-        }
+        return generalReverse(board, turn, x, y, 1, 0);
     }
     
     public static boolean reverseRight(Board board, int turn, int x, int y) {
-        int limitOpp = y;
-        int limitOwn = y;
-        
-        for (int j=y+1; j<8; j++) {
-            if (board.getBoardXY(x, j) == turn*(-1)) {
-                limitOpp = j;
-            } else if (board.getBoardXY(x, j) == turn) {
-                limitOwn = j;
-                break;
-            } else {
-                break;
-            }
-        }
-        
-        if (limitOwn>y && limitOpp>y) {
-            for (int j=y+1; j<limitOwn; j++) {
-                board.setBoardXY(x, j, turn);
-            }
-            return true;
-        } else {
-            return false;
-        }
+        return generalReverse(board, turn, x, y, 0, 1);
     }
     
     public static boolean reverseDownRight(Board board, int turn, int x, int y) {
-        int limitOwnX = x;
-        int limitOppX = x;
-        
-        int i=x+1;
-        int j=y+1;
-        
-        while (i<8 && j<8) {
-            if (board.getBoardXY(i, j) == turn*(-1)) {
-                limitOppX = i;
-            } else if (board.getBoardXY(i, j) == turn) {
-                limitOwnX = i;
-                break;
-            } else {
-                break;
-            }
-            i++;
-            j++;
-        }
-        
-        if (limitOwnX>x && limitOppX>x) {
-            i=x+1;
-            j=y+1;
-            
-            while (i<limitOwnX) {
-                board.setBoardXY(i, j, turn);
-                i++;
-                j++;
-            }
-            return true;
-        } else {
-            return false;
-        }
+        return generalReverse(board, turn, x, y, 1, 1);
     }
     
     public static boolean reverseUpLeft(Board board, int turn, int x, int y) {
-        int limitOwnX = x;
-        int limitOppX = x;
-        
-        int i=x-1;
-        int j=y-1;
-        
-        while (i>=0 && j>=0) {
-            if (board.getBoardXY(i, j) == turn*(-1)) {
-                limitOppX = i;
-            } else if (board.getBoardXY(i, j) == turn) {
-                limitOwnX = i;
-                break;
-            } else {
-                break;
-            }
-            i--;
-            j--;
-        }
-        
-        if (limitOwnX<x && limitOppX<x) {
-            i=x-1;
-            j=y-1;
-            
-            while (i>limitOwnX) {
-                board.setBoardXY(i, j, turn);
-                i--;
-                j--;
-            }
-            return true;
-        } else {
-            return false;
-        }
+        return generalReverse(board, turn, x, y, -1, -1);
     }
     
     public static boolean reverseDownLeft(Board board, int turn, int x, int y) {
-        int limitOwnX = x;
-        int limitOppX = x;
-        
-        int i=x+1;
-        int j=y-1;
-        
-        while (i<8 && j>=0) {
-            if (board.getBoardXY(i, j) == turn*(-1)) {
-                limitOppX = i;
-            } else if (board.getBoardXY(i, j) == turn) {
-                limitOwnX = i;
-                break;
-            } else {
-                break;
-            }
-            i++;
-            j--;
-        }
-        
-        if (limitOwnX>x && limitOppX>x) {
-            i=x+1;
-            j=y-1;
-            
-            while (i<limitOwnX) {
-                board.setBoardXY(i, j, turn);
-                i++;
-                j--;
-            }
-            return true;
-        } else {
-            return false;
-        }
+        return generalReverse(board, turn, x, y, 1, -1);
     }
     
     public static boolean reverseUpRight(Board board, int turn, int x, int y) {
-        int limitOwnX = x;
-        int limitOppX = x;
-        
-        int i=x-1;
-        int j=y+1;
-        
-        while (i>=0 && j<8) {
-            if (board.getBoardXY(i, j) == turn*(-1)) {
-                limitOppX = i;
-            } else if (board.getBoardXY(i, j) == turn) {
-                limitOwnX = i;
-                break;
-            } else {
-                break;
-            }
-            i--;
-            j++;
-        }
-        
-        if (limitOwnX<x && limitOppX<x) {
-            i=x-1;
-            j=y+1;
-            
-            while (i>limitOwnX) {
-                board.setBoardXY(i, j, turn);
-                i--;
-                j++;
-            }
-            return true;
-        } else {
-            return false;
-        }
+        return generalReverse(board, turn, x, y, -1, 1);
     }
     
 }
