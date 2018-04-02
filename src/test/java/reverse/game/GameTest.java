06cf17f82010514dad987ad44cfab74d51077de6
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-04-02 22:06:42 +0300

    made test for getAvailableMoves

--- src/test/java/reverse/game/GameTest.java
+++ src/test/java/reverse/game/GameTest.java
@@ -22,6 +22,23 @@ public class GameTest {
     public void setUp() {
         this.game = new Game();
     }
+    
+    @Test
+    public void availableMovesWorks() {
+        Board board = new Board();
+        board.setBoard("   0 1 2 3 4 5 6 7 \n" +
+                       "0 | | | | | | | | |\n" +
+                       "1 | | | | | | | | |\n" +
+                       "2 | | |●| |○| | | |\n" +
+                       "3 |○| |●|○|○| | | |\n" +
+                       "4 | |○|●|○|○|○|○| |\n" +
+                       "5 |○| |●| | | | | |\n" +
+                       "6 |○|○|●|●| | | | |\n" +
+                       "7 |○| |○| | | | | |\n");
+        game.setBoard(board);
+        game.setTurn(1);
+        assertEquals(9, game.getAvailableMoves().size());
+    }
    
     
     
