bd4177ac9e014b66f157941f1be5a6e0321cc1dc
Piiroinen Annika E <aepiiroi@da5-cs-b221-07.pc.helsinki.fi>
2018-04-06 17:44:50 +0300

    made boardfactory

---
+++ src/main/java/reversi/game/BoardFactory.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reversi.game;
+
+/**
+ *
+ * @author aepiiroi
+ */
+public class BoardFactory {
+    
+    private BoardFactory() {}
+    
+    public static Board makeBoard(String boardRepresentation) {
+        
+        Board board = new Board();
+        String boardString = board.toString();
+        int k = 20;
+        int c;
+
+        for (int i=0; i<8; i++) {
+            k += 3;
+            for (int j=0; j<8; j++) {
+               c = boardRepresentation.charAt(k);
+                switch (c) {
+                    case ' ':
+                        board.setBoardXY(i, j, 0);
+                        break;
+                    case '\u25CF':
+                        board.setBoardXY(i, j, -1);
+                        break;
+                    case '\u25CB':
+                        board.setBoardXY(i, j, 1);
+                        break;
+                    default:
+                        //throw something
+                        break;
+                }
+                boardString = board.toString();
+                k += 2; 
+            }
+            k += 1;
+        }
+        return board;
+    }
+    
+}
---
+++ src/test/java/reversi/game/BoardFactoryTest.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reversi.game;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ *
+ * @author aepiiroi
+ */
+public class BoardFactoryTest {
+    
+    @Test
+    public void setBoardWorks() {
+        Board board = BoardFactory.makeBoard
+                      ("   0 1 2 3 4 5 6 7 \n" +
+                       "0 | | | | | | | | |\n" +
+                       "1 | | | | | | | | |\n" +
+                       "2 | | |●| |○| | | |\n" +
+                       "3 |○| |●|○|○| | | |\n" +
+                       "4 | |○|●|○|○|○|○| |\n" +
+                       "5 |○| |●| | | | | |\n" +
+                       "6 |○|○|●|●| | | | |\n" +
+                       "7 |○| |○| | | | | |\n");
+        assertEquals("   0 1 2 3 4 5 6 7 \n" +
+                     "0 | | | | | | | | |\n" +
+                     "1 | | | | | | | | |\n" +
+                     "2 | | |●| |○| | | |\n" +
+                     "3 |○| |●|○|○| | | |\n" +
+                     "4 | |○|●|○|○|○|○| |\n" +
+                     "5 |○| |●| | | | | |\n" +
+                     "6 |○|○|●|●| | | | |\n" +
+                     "7 |○| |○| | | | | |\n", board.toString());
+    }
+}
