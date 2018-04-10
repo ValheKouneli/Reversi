761dd4f889d7967dc9523060dc672651988f0297
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-04-11 00:03:02 +0300

    created class Tree

---
+++ src/main/java/reversi/data_structures/Tree.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reversi.data_structures;
+
+/**
+ *
+ * @author Valhe Kouneli
+ */
+public class Tree {
+    
+    Node root;
+    
+    public Tree() {
+        root = new Node();
+    }
+    
+    public Node getRoot() {
+        return root;
+    }
+    
+}
