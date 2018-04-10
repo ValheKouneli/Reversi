8f8fd80a20b4c1df651df24fef0e1f2c13c3aa74
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-04-11 00:05:10 +0300

    created package reversi.AI.MTCS and moved Node and Tree there

--- src/main/java/reversi/AI/MCTS/Node.java
+++ src/main/java/reversi/AI/MCTS/Node.java
@@ -3,7 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package reversi.data_structures;
+package reversi.AI.MCTS;
+
+import reversi.data_structures.List;
 
 /**
  *
