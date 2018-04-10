e043b67c4dc3ad1685aee5f9ebc1eb8794bd4410
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-04-11 00:52:58 +0300

    stub methods

--- src/main/java/reversi/AI/MCTS/MonteCarloThreeSearch.java
+++ src/main/java/reversi/AI/MCTS/MonteCarloThreeSearch.java
@@ -16,6 +16,9 @@ public class MonteCarloThreeSearch {
     int level;
     
     public Game findNextMove(Game game) {
+        long end = System.currentTimeMillis() + 1000;
+        //how long to continue before selecting final move
+        
         throw new java.lang.UnsupportedOperationException("Not supported yet.");
     }
     
@@ -31,4 +34,15 @@ public class MonteCarloThreeSearch {
         throw new java.lang.UnsupportedOperationException("Not supported yet.");
     }
     
+    private int simulateRandomPlayout(Node node) {
+        throw new java.lang.UnsupportedOperationException("Not supported yet.");
+    }
+    
+    private void backPropogation(Node nodeToExplore, int playoutResult) {
+        throw new java.lang.UnsupportedOperationException("Not supported yet.");
+    }
+    
+    private void expandNode(Node node) {
+        throw new java.lang.UnsupportedOperationException("Not supported yet.");
+    }
 }
