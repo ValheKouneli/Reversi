3dc91587a03a6b53c85ecf42f7c86b3ed6ab538b
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-04-11 01:03:46 +0300

    added credentials/copyright info

--- src/main/java/reversi/AI/MCTS/State.java
+++ src/main/java/reversi/AI/MCTS/State.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Implemented following the tutorial on http://www.baeldung.com/java-monte-carlo-tree-search
  */
 package reversi.AI.MCTS;
 
