5a8ebfc624755bff0763226208de2a3ecb32f269
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-04-19 20:29:16 +0300

    made own package for test data

---
+++ src/test/java/testData/TestData.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testData;
+
+/**
+ *
+ * @author Valhe Kouneli
+ */
+public class TestData {
+    
+    private String boardSituation1 = "   0 1 2 3 4 5 6 7 \n" +
+                                     "0 |●|●|●|●|●|●|●|●|\n" +
+                                     "1 |○|○|○|○|○|○|●|●|\n" +
+                                     "2 |○|○|●|○|●|●|●|●|\n" +
+                                     "3 |○|○|○|●|○|●|●|●|\n" +
+                                     "4 |○|○|●|○|●|○|●|●|\n" +
+                                     "5 |○|○|○|○|●|●|●|●|\n" +
+                                     "6 |○|○|○|○|●|●| |●|\n" +
+                                     "7 |●|●|●|●|●|○| |●|\n";
+    
+    
+}
