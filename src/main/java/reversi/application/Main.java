252d3681162a60b6c9ce240a5b3d7addade992be
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-04-23 18:26:36 +0300

    made Main use my CLI class

--- src/main/java/reversi/application/Main.java
+++ src/main/java/reversi/application/Main.java
@@ -9,8 +9,7 @@ import static java.lang.Integer.parseInt;
 import java.util.Scanner;
 import reversi.AI.*;
 import reversi.AI.MCTS.MCTSbot;
-import reversi.controller.CLI;
-import reversi.data_structures.IntPair;
+import CLI.CLI;
 import reversi.data_structures.Pair;
 import reversi.game.Reversi;
 
@@ -24,28 +23,31 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Reversi game = new Reversi();
-        MCTSbot ai1 = new MCTSbot(500);
-        MinimaxAI ai2 = new MinimaxAI(new ReversiEvaluator2());
         
-        Scanner reader = new Scanner(System.in);
-        
-         int i = 0;
-         Pair move;
- 
-        do {
-            System.out.println(game.toString());
-            if (game.getTurn() == 1) {
-                game.move(ai1.getNextMove(game));
-            } else {
-                game.move(ai2.getNextMove(game));
-            }
-            i++;
-        } while (!game.getMoves().isEmpty());
-        
-        System.out.println(game.toString());
-         
-      
+        CLI cli = new CLI();
+        cli.run();
+//        Reversi game = new Reversi();
+//        MCTSbot ai1 = new MCTSbot(500);
+//        MinimaxAI ai2 = new MinimaxAI(new ReversiEvaluator2());
+//        
+//        Scanner reader = new Scanner(System.in);
+//        
+//         int i = 0;
+//         Pair move;
+// 
+//        do {
+//            System.out.println(game.toString());
+//            if (game.getTurn() == 1) {
+//                game.move(ai1.getNextMove(game));
+//            } else {
+//                game.move(ai2.getNextMove(game));
+//            }
+//            i++;
+//        } while (!game.getMoves().isEmpty());
+//        
+//        System.out.println(game.toString());
+//         
+//      
     }
     
     private static void humanGetMove(Reversi game, Scanner reader) {
