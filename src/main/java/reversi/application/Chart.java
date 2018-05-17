92fb2ac4ca6a9410286672e071aa37d9c8b2fbaa
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-05-17 19:21:17 +0300

    added comments, did rearranging

--- src/main/java/reversi/application/Chart.java
+++ src/main/java/reversi/application/Chart.java
@@ -12,6 +12,7 @@ import javafx.stage.Stage;
 import java.util.concurrent.CountDownLatch;
 import javafx.application.Platform;
 import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.chart.Axis;
 import javafx.scene.image.WritableImage;
 import javafx.stage.WindowEvent;
 import javax.imageio.ImageIO;
@@ -32,54 +33,84 @@ public class Chart extends Application {
     }
  
     @Override public void start(Stage stage) throws Exception {
-        stage.setTitle("MCTS Bot vs. Minimax Ai");
-        //defining the axes
-        final NumberAxis xAxis = new NumberAxis();
-        final NumberAxis yAxis = new NumberAxis();
-        xAxis.setLabel("Minimax AI search depth");
-        yAxis.setLabel("MCTS Bot win %");
-        //creating the chart
-        final LineChart<Number,Number> lineChart = 
-                new LineChart<>(xAxis,yAxis);
-                
-        lineChart.setTitle("MCTS Bot vs. Minimax Ai");
-
-        XYChart.Series series = new XYChart.Series();
-        series.setName("Test run results read from file");
         
-        File results = new File("raw_data/results.txt");
-        try (Scanner input = new Scanner(results)) {
-            input.useDelimiter("\\s+");
-            int x;
-            int y;
-            int next;
+        try {
+            File results = new File("raw_data/results.txt");
             
-            while(input.hasNextInt()) {
-                next = input.nextInt();
-                x = next;
+            try {
+                Scanner input = new Scanner(results);
+                input.useDelimiter("\\s+");
+
+                /*
+                * Reads min x vale from file. Value is not used atm.
+                */
+                int minX = 0;
                 if (input.hasNextInt()) {
-                    next = input.nextInt();
-                    y = next;
-                    series.getData().add(new XYChart.Data(x, y));
-                } else {
-                    throw new java.lang.Exception("There was an odd number of entries"
-                            + " in results.txt!");
+                    minX = input.nextInt();
                 }
+
+                //set title
+                stage.setTitle("MCTS Bot vs. Minimax Ai");
+
+                //defining the axises
+                final NumberAxis xAxis = new NumberAxis();
+                final NumberAxis yAxis = new NumberAxis();
+                xAxis.setLabel("Minimax AI search depth");
+                yAxis.setLabel("MCTS Bot win %");
+
+                //creating the chart
+                final LineChart<Number,Number> lineChart = 
+                        new LineChart<>(xAxis,yAxis);
+                lineChart.setTitle("MCTS Bot vs. Minimax Ai");
+
+                //initialize data series
+                XYChart.Series series = new XYChart.Series();
+                series.setName("MCTS Bot win percentage against "
+                        + "Minimax AI related to minimax search depth");
+
+                /*
+                * Read actual data from file. Chart data
+                */
+                int x;
+                int y;
+                int next;
+
+                while(input.hasNextInt()) {
+                    next = input.nextInt();
+                    x = next;
+                    if (input.hasNextInt()) {
+                        next = input.nextInt();
+                        y = next;
+                        series.getData().add(new XYChart.Data(x, y));
+                    } else {
+                        //should never happen
+                        throw new java.lang.Exception("There was an odd "
+                                + "number of entries in results.txt!");
+                    }
+                }
+
+                Scene scene  = new Scene(lineChart,800,600);
+                lineChart.getData().add(series);
+
+                stage.setScene(scene);
+                stage.setOnCloseRequest((WindowEvent t) -> {
+                    Platform.exit();
+                    System.exit(0);
+                });
+                stage.show();
+
+            } catch (Exception e) {
+                System.out.println("Could not open the result file: " + e + "\n" +
+                        Arrays.toString(e.getStackTrace()));
             }
+        
         } catch (Exception e) {
-            System.out.println("Could not open the result file: " + e + "\n" +
-                    Arrays.toString(e.getStackTrace()));
+            System.out.println("Could not create a file.\\n"
+                    + e.toString());
         }
         
-        Scene scene  = new Scene(lineChart,800,600);
-        lineChart.getData().add(series);
-       
-        stage.setScene(scene);
-        stage.setOnCloseRequest((WindowEvent t) -> {
-            Platform.exit();
-            System.exit(0);
-        });
-        stage.show();
+        
+
         
 //        lineChart.setAnimated(true);
 //        WritableImage snapShot = scene.snapshot(null);
