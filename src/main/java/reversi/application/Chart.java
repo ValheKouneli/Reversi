0d33cc397173cb311a8d49c649aa79c868f6ceba
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-05-17 21:51:20 +0300

    results are appended in raw_data/legacy_results.txt and chart is made based on that

--- src/main/java/reversi/application/Chart.java
+++ src/main/java/reversi/application/Chart.java
@@ -1,7 +1,10 @@
 package reversi.application;
 
 import java.io.File;
-import java.util.Arrays;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -12,97 +15,112 @@ import javafx.stage.Stage;
 import java.util.concurrent.CountDownLatch;
 import javafx.application.Platform;
 import javafx.stage.WindowEvent;
+import reversi.data_structures.IntPair;
  
  
 public class Chart extends Application {
     
     public static final CountDownLatch LATCH = new CountDownLatch(1);
     public static Chart chart = null;
-    
-    public static Chart waitForChart() {
-        try {
-            LATCH.await();
-        } catch (InterruptedException e) {
-            System.out.println("Something unextected happened: \n" + e.toString());
-        }
-        return chart;
-    }
  
     @Override public void start(Stage stage) throws Exception {
+
+        //set title
+        stage.setTitle("MCTS Bot vs. Minimax Ai");
+
+        //defining the axises
+        final NumberAxis xAxis = new NumberAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel("Minimax AI search depth");
+        yAxis.setLabel("MCTS Bot win %");
+
+        //creating the chart
+        final LineChart<Number,Number> lineChart = 
+                new LineChart<>(xAxis,yAxis);
+        lineChart.setTitle("MCTS Bot vs. Minimax Ai");
+
+        //initialize data series
+        XYChart.Series series = new XYChart.Series();
+        series.setName("MCTS Bot win percentage against "
+                + "Minimax AI related to minimax search depth");
+        
+        HashMap<Integer, IntPair> collectedData;
+        collectedData = new HashMap<>();
         
         try {
-            File results = new File("raw_data/results.txt");
+            File results = new File("raw_data/legacy_results.txt");
             
             try {
                 Scanner input = new Scanner(results);
                 input.useDelimiter("\\s+");
 
                 /*
-                * Reads min x vale from file. Value is not used atm.
-                */
-                int minX = 0;
-                if (input.hasNextInt()) {
-                    minX = input.nextInt();
-                }
-
-                //set title
-                stage.setTitle("MCTS Bot vs. Minimax Ai");
-
-                //defining the axises
-                final NumberAxis xAxis = new NumberAxis();
-                final NumberAxis yAxis = new NumberAxis();
-                xAxis.setLabel("Minimax AI search depth");
-                yAxis.setLabel("MCTS Bot win %");
-
-                //creating the chart
-                final LineChart<Number,Number> lineChart = 
-                        new LineChart<>(xAxis,yAxis);
-                lineChart.setTitle("MCTS Bot vs. Minimax Ai");
-
-                //initialize data series
-                XYChart.Series series = new XYChart.Series();
-                series.setName("MCTS Bot win percentage against "
-                        + "Minimax AI related to minimax search depth");
-
-                /*
                 * Read actual data from file. Chart data
                 */
-                int x;
-                int y;
-                int next;
 
-                while(input.hasNextInt()) {
-                    next = input.nextInt();
-                    x = next;
-                    if (input.hasNextInt()) {
-                        next = input.nextInt();
-                        y = next;
-                        series.getData().add(new XYChart.Data(x, y));
-                    } else {
-                        //should never happen
-                        throw new java.lang.Exception("There was an odd "
-                                + "number of entries in results.txt!");
+                int weight;
+                int depth;
+                int winPercentage;
+
+                try {
+                    while(input.hasNextInt()) {
+                        //System.out.println("Reading data...");
+                        weight = input.nextInt();
+                        depth = input.nextInt();
+                        winPercentage = input.nextInt();
+                        //System.out.println("Read weight " + weight + ", " +
+                        //        depth + ", " + winPercentage + "%");
+                        if (!collectedData.keySet().contains(depth)) {
+                            //System.out.println("Inserting new depth");
+                            IntPair data = 
+                                    new IntPair((Integer) weight, weight*winPercentage);
+                            collectedData.put(depth, data);
+                        } else {
+                            //System.out.println("Updating new depth");
+                            IntPair data = collectedData.get(depth);
+                            collectedData.put((Integer) depth, 
+                                    new IntPair((int)data.getX()+weight, 
+                                            data.getY()+weight*winPercentage));
+                        }
                     }
+                } catch (NoSuchElementException e) {
+                    System.out.println("Result file formatting is corrupted.\n"
+                            + "No chart is being drawn.");
+                    System.exit(1);
                 }
 
-                Scene scene  = new Scene(lineChart,800,600);
-                lineChart.getData().add(series);
 
-                stage.setScene(scene);
-                stage.setOnCloseRequest((WindowEvent t) -> {
-                    Platform.exit();
-                    System.exit(0);
-                });
-                stage.show();
-
-            } catch (Exception e) {
-                System.out.println("Could not open the result file: " + e + "\n" +
-                        Arrays.toString(e.getStackTrace()));
+            } catch (FileNotFoundException e) {
+                System.out.println("Result file was not found.\n" + 
+                        e.toString());
+                System.exit(1);
             }
         
         } catch (Exception e) {
             System.out.println("Could not create a file.\\n"
                     + e.toString());
+            System.exit(1);
         }
+        
+        int avgPercentage;
+       
+        for (Integer key : collectedData.keySet()) {
+            avgPercentage = collectedData.get(key).getY() /
+                    collectedData.get(key).getX();
+            //System.out.println("Charting value pair x: " + key +
+            //       ", y: " + avgPercentage);
+            series.getData().add(new XYChart.Data(key, avgPercentage));
+        }
+
+
+        Scene scene  = new Scene(lineChart,800,600);
+        lineChart.getData().add(series);
+
+        stage.setScene(scene);
+        stage.setOnCloseRequest((WindowEvent t) -> {
+            Platform.exit();
+            System.exit(0);
+        });
+        stage.show();
     }
 }
--- src/main/java/reversi/application/Main.java
+++ src/main/java/reversi/application/Main.java
@@ -1,10 +1,17 @@
+//
+
+
 package reversi.application;
 
 
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 import reversi.AI.MCTS.MCTSBot;
 import reversi.AI.minimax.MinimaxAI;
 import reversi.data_structures.IntPair;
@@ -27,71 +34,182 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         final int DEFAULT_MAX_DEPTH = 3;
-        final int DEFAULT_MIN_DEPTH = 2;
+        final int DEFAULT_MIN_DEPTH = 1;
+        final int DEFAULT_MATCH_NUMBER = 10;
         int maxDepth = DEFAULT_MAX_DEPTH;
         int minDepth = DEFAULT_MIN_DEPTH;
+        int matchNumber = DEFAULT_MATCH_NUMBER;
         
-        if (args.length >= 2) {
-            try {
-                maxDepth = Integer.parseInt(args[1]);
-                if (maxDepth<DEFAULT_MIN_DEPTH) {
-                    maxDepth = DEFAULT_MIN_DEPTH;
-                }
-            } catch (NumberFormatException e) {
-                
-            }
+        /*
+        * Handle args
+        */
+        
+        switch (args.length) {
+        //currently set minDepth, maxDepth and matchNumber are ok
+            case 0:
+                break;
+            case 1:
+                try {
+                    maxDepth = Integer.parseInt(args[0]);
+                    if (maxDepth<DEFAULT_MIN_DEPTH) {
+                        throw new IllegalArgumentException("Max depth (given "
+                                + "argument) must be at least " +
+                                DEFAULT_MIN_DEPTH + ".");
+                    }
+                } catch (NumberFormatException e) {
+                    respondToBadArguments();
+                }   break;
+            case 2:
+                try {
+                    minDepth = Integer.parseInt(args[0]);
+                    if (minDepth<1 ) {
+                        throw new IllegalArgumentException("Min depth (1st "
+                                + "argument) must be at least 1.");
+                    }
+                    maxDepth = Integer.parseInt(args[1]);
+                    if (maxDepth<minDepth) {
+                        throw new IllegalArgumentException("Max depth (2rd "
+                                + "argument) must be greater than min depth "
+                                + "(1st argument).");
+                    }
+                } catch (NumberFormatException e) {
+                    respondToBadArguments();
+                }   break;
+            case 3:
+                try {
+                    minDepth = Integer.parseInt(args[0]);
+                    if (minDepth<1 ) {
+                        throw new IllegalArgumentException("Min depth (1st "
+                                + "argument) must be at least 1.");
+                    }
+                    maxDepth = Integer.parseInt(args[1]);
+                    if (maxDepth<minDepth) {
+                        throw new IllegalArgumentException("Max depth (2rd "
+                                + "argument) must be greater than min depth "
+                                + "(1st argument).");
+                    }
+                    matchNumber = Integer.parseInt(args[2]);
+                    if (matchNumber < 2 || matchNumber%2 == 1) {
+                        throw new IllegalArgumentException("Match number (3rd "
+                                + "argument) must be even and at least 2.");
+                    }
+                } catch (NumberFormatException e) {
+                    respondToBadArguments();
+                }   break;
+            default:
+                respondToBadArguments();
+                break;
         }
-        List<IntPair> record = new List<>();
         
-        for (int depth = DEFAULT_MIN_DEPTH; depth<=maxDepth; depth++) {
-            MinimaxAI minimax = new MinimaxAI(new ReversiEvaluator(), depth);
-            RecordMoveTimeMinimax timing = new RecordMoveTimeMinimax(minimax);
-            int avgTime = timing.record(true);
-            System.out.println(timing.toString());
-            MCTSBot mcts = new MCTSBot(avgTime);
-            ScoreKeeper score = new ScoreKeeper(minimax, mcts);
-            for (int i=0; i<5; i++) {
-                score.playSwitchingColors(true);
-            }
-            System.out.println(score.toString());
-            record.add(new IntPair(depth, score.getMctsBotWins() * 100 / 
-                    score.getTotalMatches()));
+        playBotsAgainstEachOtherAndRecord(minDepth, maxDepth, matchNumber);
+            
+        printLatestResults();
+        
+        javafx.application.Application.launch(Chart.class);
 
-        }
-        
+    }
+    
+    /*
+    * Go through all different depths and make Minimax AI and MCTS Bot
+    * play against each other 'matchNumber' amount of times.
+    */
+    private static void playBotsAgainstEachOtherAndRecord(int minDepth, 
+            int maxDepth, int matchNumber) {
+
+        try {
+            PrintWriter writerNew = new PrintWriter(new BufferedWriter(
+                new FileWriter("raw_data/latest_results.txt", false)));
+            PrintWriter writerAppend = new PrintWriter(new BufferedWriter(
+                new FileWriter("raw_data/legacy_results.txt", true)));
+            
+            for (int depth = minDepth; depth<=maxDepth; depth++) {
+                //create MinimaxBot
+                MinimaxAI minimax = new MinimaxAI(new ReversiEvaluator(), depth);
+                //create 'timing' to record MinimaxBot's average move time
+                RecordMoveTimeMinimax timing = new RecordMoveTimeMinimax(minimax);
+
+                int avgTime = timing.record(true);
+                //print Minimax Bot's avg move time
+                System.out.println(timing.toString());
+
+                //create MCTS Bot
+                MCTSBot mcts = new MCTSBot(avgTime);
+
+                //create 'score' for keeping score on
+                //how the bots perform against each other
+                ScoreKeeper score = new ScoreKeeper(minimax, mcts);
+
+                //play 'matchNumber' of matches
+                for (int i=0; i<matchNumber/2; i++) {
+                    score.playSwitchingColors(true);
+                }
+
+                //prints the results for this depth
+                System.out.println(score.toString());
+                
+                //writes the results in file
+                int winPercentage = score.getMctsBotWins() * 100 / 
+                        score.getTotalMatches();
+                
+                record(writerNew, matchNumber, depth, winPercentage);
+                record(writerAppend, matchNumber, depth, winPercentage);
+            }
+            
+            writerNew.close();
+            writerAppend.close();
+        } catch (IOException e) {
+            System.out.println("Writing to file did not succeed.\n +"
+                    + " e.toString()");
+        } 
+    }
+    
+    private static void printLatestResults() {
         System.out.println("MCTS Bot win percentages\n" +
                 "against Minimax Bot with\n");
-        int depth;
-        int winPercentage;
-        for (int i=0; i<record.size(); i++) {
-            depth = record.get(i).getX();
-            winPercentage = record.get(i).getY();
-            System.out.println("depth " + depth + ": " + winPercentage + "%");
-        }
-  
-        try (PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter("raw_data/results.txt", false)));) {
-            writer.print(Integer.toString(minDepth));
-            writer.println("\n");
-            
-            for (int i=0; i<record.size(); i++) {
-                writer.print(Integer.toString(record.get(i).getX()));
-                writer.print(" ");
-                writer.print(Integer.toString(record.get(i).getY()));
-                writer.print("\n");
-            }
-            writer.close();
-        } catch (Exception e) {
-            System.out.println("Writing to file did not succeed.");
-        }
-
         
-        new Thread() {
-            @Override
-            public void run() {
-                javafx.application.Application.launch(Chart.class);
+        File results = new File("raw_data/latest_results.txt");
+        if(results.isFile()) { 
+            try {
+                Scanner input = new Scanner(results);
+                input.useDelimiter("\\s+");
+
+                try {
+                    while(input.hasNextInt()) {
+                        input.nextInt();
+                        System.out.println("depth " + input.nextInt() + ": " +
+                        "win percentage " + input.nextInt() + "%");
+                    }
+                } catch (NoSuchElementException e) {
+                    System.out.println("Result file formatting is corrupted.");
+                }
+ 
+            } catch (FileNotFoundException e) {
+                System.out.println("Could not find the result file.\n" +
+                        e.toString());
             }
-        }.start();
-        Chart chart = Chart.waitForChart();
+        } else {
+            throw new java.lang.IllegalStateException("There should be a "
+                    + "result file to read.");
+        }
+    }
+    
+    private static void respondToBadArguments() {
+        String instructions = "Program takes 0-3 arguments. "
+                        + "Arguments must be integers.";
+        throw new IllegalArgumentException(instructions);
+    }
+
+    /*
+    * writes results to file with given writer
+    */
+    private static void record(PrintWriter writer, 
+            int matchNumber, int depth, int winPercentage) {
+        writer.print(Integer.toString(matchNumber));
+        writer.print(" ");
+        writer.print(Integer.toString((depth)));
+        writer.print(" ");
+        writer.print(Integer.toString(winPercentage));
+        writer.print("\n");
     }
     
 }
