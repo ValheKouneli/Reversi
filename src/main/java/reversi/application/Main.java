7f1132d54fca627a93f720f2f2db32acb8bbf6c1
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-05-17 17:15:28 +0300

    Now Main writes data into a file and also runs an application that shows a chart drawn based on that file

--- src/main/java/reversi/application/Main.java
+++ src/main/java/reversi/application/Main.java
@@ -1,7 +1,14 @@
 package reversi.application;
 
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import reversi.AI.MCTS.MCTSBot;
 import reversi.AI.minimax.MinimaxAI;
+import reversi.data_structures.IntPair;
+import reversi.data_structures.List;
 import reversi.game.reversi.ReversiEvaluator;
 
 
@@ -16,12 +23,15 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
         
-        for (int depth = 3; depth<=5; depth++) {
+        List<IntPair> record = new List<>();
+        
+        for (int depth = 3; depth<=3; depth++) {
             MinimaxAI minimax = new MinimaxAI(new ReversiEvaluator(), depth);
             RecordMoveTimeMinimax timing = new RecordMoveTimeMinimax(minimax);
-            int avgTime = timing.record();
+            int avgTime = timing.record(true);
             System.out.println(timing.toString());
             MCTSBot mcts = new MCTSBot(avgTime);
             ScoreKeeper score = new ScoreKeeper(minimax, mcts);
@@ -29,8 +39,41 @@ public class Main {
                 score.playSwitchingColors(true);
             }
             System.out.println(score.toString());
+            record.add(new IntPair(depth, score.getMctsBotWins() * 100 / 
+                    score.getTotalMatches()));
+
+        }
+        
+        System.out.println("MCTS Bot win percentages\n" +
+                "against Minimax Bot with\n");
+        int depth;
+        int winPercentage;
+        for (int i=0; i<record.size(); i++) {
+            depth = record.get(i).getX();
+            winPercentage = record.get(i).getY();
+            System.out.println("depth " + depth + ": " + winPercentage + "%");
+        }
+  
+        try (PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter("results.txt", false)));) {
+            for (int i=0; i<record.size(); i++) {
+                writer.print(Integer.toString(record.get(i).getX()));
+                writer.print(" ");
+                writer.print(Integer.toString(record.get(i).getY()));
+                writer.print("\n");
+            }
+            writer.close();
+        } catch (Exception e) {
+            System.out.println("Writing to file did not succeed.");
         }
 
+        
+        new Thread() {
+            @Override
+            public void run() {
+                javafx.application.Application.launch(Chart.class);
+            }
+        }.start();
+        Chart chart = Chart.waitForChart();
     }
     
 }
